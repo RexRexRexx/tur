ed6414d56f1133d353bed5c752648a0891afbab1
amigo <sites.hankering681@simplelogin.com>
2025-12-24 21:38:08 +0300

    Fix: Remove non-existent python-joblib dependency

--- tur/python-scikit-learn/build.sh
+++ tur/python-scikit-learn/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@RexRexRexx"
 TERMUX_PKG_VERSION=1.6.1
 TERMUX_PKG_SRCURL=https://github.com/scikit-learn/scikit-learn/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ff4a3272d5c6c0df6ef42a5ab0d11a4bac61505568f7f4a12c7f7bf0c9e2d978
-TERMUX_PKG_DEPENDS="python, python-numpy, python-scipy, python-joblib"
+TERMUX_PKG_DEPENDS="python, python-numpy, python-scipy"
 TERMUX_PKG_BUILD_DEPENDS="python-numpy-static, python-scipy-static, pybind11, cython, clang"
 
 # Optional: Uncomment if you only want ARM64
