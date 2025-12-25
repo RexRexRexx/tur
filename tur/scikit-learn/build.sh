2de9a6c08c7de0d3d048b75b976f13c483e513b9
amigo <sites.hankering681@simplelogin.com>
2025-12-25 20:12:41 +0300

    fix: remove lapack dependency

--- tur/scikit-learn/build.sh
+++ tur/scikit-learn/build.sh
@@ -5,13 +5,13 @@ TERMUX_PKG_MAINTAINER="@RexRexRexx"
 TERMUX_PKG_VERSION="1.3.2"
 TERMUX_PKG_SRCURL=https://files.pythonhosted.org/packages/source/s/scikit-learn/scikit-learn-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a2f54c76accc15a34bfb9066e6c7a56c1e7235dda5762b990792330b52ccfb05
-TERMUX_PKG_DEPENDS="python, libblas, liblapack"
+TERMUX_PKG_DEPENDS="python"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, 'Cython<3.0', numpy, scipy, joblib, threadpoolctl"
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, Cython==0.29.36, numpy, scipy, joblib, threadpoolctl"
 
 termux_step_pre_configure() {
 	export SKLEARN_NO_OPENMP=1
-	LDFLAGS+=" -lpython${TERMUX_PYTHON_VERSION} -lblas -llapack"
+	LDFLAGS+=" -lpython${TERMUX_PYTHON_VERSION}"
 }
 
 termux_step_make() {
