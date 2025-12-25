2fc0f8c89b062cbfbc0f938373b0b9c1da856f40
amigo <sites.hankering681@simplelogin.com>
2025-12-25 21:39:13 +0300

    fix: use sed to apply the patch

--- tur/scikit-learn/build.sh
+++ tur/scikit-learn/build.sh
@@ -10,26 +10,26 @@ TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, setuptools, Cython==0.29.36, scipy, joblib, threadpoolctl"
 
 termux_step_post_get_source() {
-    # Fix int_t compatibility issue
-    # Replace cnp.int_t with cnp.intp_t throughout the codebase
-    find sklearn -name "*.pyx" -o -name "*.pxd" | xargs sed -i 's/cnp\.int_t/cnp.intp_t/g'
-    find sklearn -name "*.pyx" -o -name "*.pxd" | xargs sed -i 's/\bint_t\b/intp_t/g'
+	# Fix int_t compatibility issue
+	# Replace cnp.int_t with cnp.intp_t throughout the codebase
+	find sklearn -name "*.pyx" -o -name "*.pxd" | xargs sed -i 's/cnp\.int_t/cnp.intp_t/g'
+	find sklearn -name "*.pyx" -o -name "*.pxd" | xargs sed -i 's/\bint_t\b/intp_t/g'
 }
 
 termux_step_pre_configure() {
-    export SKLEARN_NO_OPENMP=1
-    export SKLEARN_BUILD_PARALLEL=1
-    
-    export CFLAGS="${CFLAGS} -Wno-unreachable-code"
-    export CXXFLAGS="${CXXFLAGS} -Wno-unreachable-code"
-    
-    LDFLAGS+=" -lpython${TERMUX_PYTHON_VERSION}"
+	export SKLEARN_NO_OPENMP=1
+	export SKLEARN_BUILD_PARALLEL=1
+
+	export CFLAGS="${CFLAGS} -Wno-unreachable-code"
+	export CXXFLAGS="${CXXFLAGS} -Wno-unreachable-code"
+
+	LDFLAGS+=" -lpython${TERMUX_PYTHON_VERSION}"
 }
 
 termux_step_make() {
-    python setup.py bdist_wheel -d "${TERMUX_PKG_BUILDDIR}"
+	python setup.py bdist_wheel -d "${TERMUX_PKG_BUILDDIR}"
 }
 
 termux_step_make_install() {
-    pip install --no-deps --prefix="${TERMUX_PREFIX}" "${TERMUX_PKG_BUILDDIR}"/scikit_learn-*.whl
+	pip install --no-deps --prefix="${TERMUX_PREFIX}" "${TERMUX_PKG_BUILDDIR}"/scikit_learn-*.whl
 }
