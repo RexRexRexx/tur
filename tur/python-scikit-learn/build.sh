8b6f805068ab272267993cfa1c7f832b376c8892
amigo <sites.hankering681@simplelogin.com>
2025-12-25 11:42:59 +0300

    Fix: Use python-scipy build pattern with proper dependency handling

--- tur/python-scikit-learn/build.sh
+++ tur/python-scikit-learn/build.sh
@@ -1,46 +1,67 @@
-TERMUX_PKG_HOMEPAGE=https://scikit-learn.org/
+TERMUX_PKG_HOMEPAGE=https://scikit-learn.org
 TERMUX_PKG_DESCRIPTION="Machine learning in Python"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@RexRexRexx"
 TERMUX_PKG_VERSION=1.6.1
-TERMUX_PKG_SRCURL=https://github.com/scikit-learn/scikit-learn/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f4f8247b43d6ac4cffb266522fe6f7eb3ef8e0ff1c2c4c9f4db42956e56f8b37
-TERMUX_PKG_DEPENDS="python, python-numpy, python-scipy, python-joblib, python-threadpoolctl"
-TERMUX_PKG_BUILD_DEPENDS="python-cython, python-meson-python"
-TERMUX_PKG_PYTHON_COMMON_DEPS="wheel"
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL=git+https://github.com/scikit-learn/scikit-learn
+TERMUX_PKG_DEPENDS="libc++ (>= 29), libopenblas, python, python-numpy"
+TERMUX_PKG_BUILD_DEPENDS="python-numpy-static"
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, 'Cython>=3.0.4', meson-python, build, joblib, threadpoolctl"
+TERMUX_PKG_PYTHON_BUILD_DEPS="'pybind11>=2.10.4', 'numpy==$TERMUX_PYTHON_NUMPY_VERSION'"
+TERMUX_PKG_ON_DEVICE_BUILD_NOT_SUPPORTED=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
+TERMUX_PKG_EXCLUDED_ARCHES="i686"  # Optional: exclude problematic architectures
+
+TERMUX_MESON_WHEEL_CROSSFILE="$TERMUX_PKG_TMPDIR/wheel-cross-file.txt"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-Dblas=openblas
+-Dlapack=openblas
+--cross-file $TERMUX_MESON_WHEEL_CROSSFILE
+"
+
+TERMUX_PKG_RM_AFTER_INSTALL="
+bin/
+"
+
+source $TERMUX_SCRIPTDIR/common-files/setup_toolchain_gcc.sh
 
 termux_step_pre_configure() {
-	# CRITICAL: Skip meson sanity checks for cross-compilation
-	export MESONPY_SKIP_SANITY_CHECK=1
-	export SKLEARN_BUILD_PARALLEL=0
+	# Similar to scipy's fix for libunwind
+	local _unwind_dir="$TERMUX_PKG_TMPDIR/_libunwind_libdir"
+	local _NDK_ARCH=$TERMUX_ARCH
+	test $_NDK_ARCH == 'i686' && _NDK_ARCH='i386'
 
-	# Create cross-compilation config for meson
-	cat > ${TERMUX_PKG_TMPDIR}/meson-cross.ini << EOF
-[properties]
-skip_sanity_check = true
-needs_exe_wrapper = true
+	mkdir -p $_unwind_dir
+	cp $NDK/toolchains/llvm/prebuilt/linux-x86_64/lib/clang/*/lib/linux/$_NDK_ARCH/libunwind.a \
+		$_unwind_dir/libunwind.a
 
-[host_machine]
-system = 'linux'
-cpu_family = 'aarch64'
-cpu = 'aarch64'
-endian = 'little'
-EOF
+	_setup_toolchain_ndk_gcc_15
+	LDFLAGS+=" -Wl,--no-as-needed,-lpython${TERMUX_PYTHON_VERSION},--as-needed"
+	LDFLAGS="-L$TERMUX_PKG_TMPDIR/_libunwind_libdir -l:libunwind.a ${LDFLAGS}"
+}
 
-	export MESONPY_CROSS_ARGS="--cross-file ${TERMUX_PKG_TMPDIR}/meson-cross.ini"
+termux_step_configure() {
+	termux_setup_meson
 
-	# Use specific compiler flags for Android
-	export LDFLAGS+=" -lm -llog"
-	export CFLAGS+=" -fPIC"
-	export CXXFLAGS+=" -fPIC"
+	cp -f $TERMUX_MESON_CROSSFILE $TERMUX_MESON_WHEEL_CROSSFILE
+	sed -i 's|^\(\[binaries\]\)$|\1\npython = '\'$(command -v python)\''|g' \
+		$TERMUX_MESON_WHEEL_CROSSFILE
+	sed -i 's|^\(\[properties\]\)$|\1\nnumpy-include-dir = '\'$PYTHON_SITE_PKG/numpy/_core/include\''|g' \
+		$TERMUX_MESON_WHEEL_CROSSFILE
+
+	(unset PYTHONPATH && termux_step_configure_meson)
 }
 
 termux_step_make() {
-	# Build the package
-	pip install . --no-deps --no-build-isolation
+	pushd $TERMUX_PKG_SRCDIR
+	PYTHONPATH= python -m build -w -n -x --config-setting builddir=$TERMUX_PKG_BUILDDIR .
+	popd
 }
 
 termux_step_make_install() {
-	# Install to termux prefix
-	pip install . --prefix=$TERMUX_PREFIX --no-deps --no-build-isolation
+	local _pyv="${TERMUX_PYTHON_VERSION/./}"
+	local _whl="scikit_learn-${TERMUX_PKG_VERSION}-cp${_pyv}-cp${_pyv}-linux_$TERMUX_ARCH.whl"
+	pip install --no-deps --prefix=$TERMUX_PREFIX $TERMUX_PKG_SRCDIR/dist/$_whl
 }
